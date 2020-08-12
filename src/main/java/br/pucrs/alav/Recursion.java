e305c2ac76644480772fb33872ed39e309a576d5
Michael da Costa Móra <michael.c.mora@gmail.com>
2020-08-12 20:09:58 -0300

    Implementação e testes para Soma por incrementos.

--- src/main/java/br/pucrs/alav/Recursion.java
+++ src/main/java/br/pucrs/alav/Recursion.java
@@ -2,6 +2,45 @@ package br.pucrs.alav;
 
 public class Recursion {
 	public static void main(String[] args) {
-		System.out.println("Teste");
+		Recursion rec = new Recursion();
+		System.out.println(rec.multNat(6, 4));
 	}
+
+	public long multNat(long vezes, long nro) {
+		if (vezes < 0)
+			throw new IllegalArgumentException("Número negativo");
+		else if (vezes == 0)
+			return 0;
+		else if (vezes == 1)
+			return nro;
+		else 
+			return multNat(vezes - 1, nro) + nro;
+	}
+
+	public long somaIncr(long n1, long n2) {
+		if ((n1 < 0) || (n2 < 0))
+			throw new IllegalArgumentException();
+		else if ((n1 ==0) && (n2 == 0))
+			return(0);
+		else if (n1 == 0)
+			return(somaIncr(n1, n2 - 1) + 1);
+		else if (n2 == 0)
+			return(somaIncr(n1 - 1, n2) + 1);
+		else 
+			return(somaIncr(n1 - 1, n2 - 1) + 1 + 1);
+	}
+	
+	public long somaIncrAlt(long n1, long n2) {
+		if ((n1 < 0) || (n2 < 0))
+			throw new IllegalArgumentException();
+		else if ((n1 ==0) && (n2 == 0))
+			return(0);
+		else if (n1 == 0)
+			return(somaIncr(n1, n2 - 1) + 1);
+		else if (n2 == 0)
+			return(somaIncr(n1 - 1, n2) + 1);
+		else 
+			return(somaIncr(n1, n2 - 1) + 1);
+	}
+
 }
--- src/test/java/br/pucrs/alav/RecursionTests.java
+++ src/test/java/br/pucrs/alav/RecursionTests.java
@@ -2,14 +2,79 @@ package br.pucrs.alav;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-
+@TestInstance(Lifecycle.PER_CLASS)
 public class RecursionTests {
+	private Recursion rec;
+	
+	@BeforeAll
+	public void setUp() {
+		rec = new Recursion();
+	}
+	
+	@Test
+	public void testMultNat1() {
+		assertEquals(rec.multNat(6, 4), 24, "6 * 4 == 24");
+	}
+	
+	@Test
+	public void testMultNat2() {
+		assertEquals(rec.multNat(4, 6), 24, "4 * 6 == 24");
+	}
+	
+	@Test
+	public void testMultNat3() {
+		assertEquals(rec.multNat(1, 5), 05, "1 * 5 == 5");
+	}
+	
+	@Test
+	public void testMultNat4() {
+		assertEquals(rec.multNat(5, 5), 25, "5 * 5 == 25");
+	}
+	
+	@Test
+	public void testMultNat5() {
+		assertEquals(rec.multNat(0, 5), 0, "0 * 5 == 0");
+	}
+	
+	@Test
+	public void testMultNat6() {
+		assertEquals(rec.multNat(5, 0), 0, "0 * 5 == 0");
+	}
 
 	@Test
-	public void test() { 
-		assertTrue(true, "Será?");
+	public void testSomaIncr1() {
+		assertEquals(rec.somaIncr(4, 2), 6, "4 + 2 == 6");
+	}
+	
+	@Test
+	public void testSomaIncr2() {
+		assertEquals(rec.somaIncr(2, 4), 6, "2 + 4 == 6");
+	}
+
+	@Test
+	public void testSomaIncr3() {
+		assertEquals(rec.somaIncr(4, 4), 8, "4 + 4 == 8");
+	}
+
+	@Test
+	public void testSomaIncrAlt1() {
+		assertEquals(rec.somaIncrAlt(4, 2), 6, "4 + 2 == 6");
+	}
+	
+	@Test
+	public void testSomaIncrAlt2() {
+		assertEquals(rec.somaIncrAlt(2, 4), 6, "2 + 4 == 6");
+	}
+
+	@Test
+	public void testSomaIncrAlt3() {
+		assertEquals(rec.somaIncrAlt(4, 4), 8, "4 + 4 == 8");
 	}
 
 }
