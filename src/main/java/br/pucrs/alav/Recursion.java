177871db083303f94b0a099f9ce1e16443df06fe
Michael da Costa Móra <michael.c.mora@gmail.com>
2020-08-12 16:46:27 -0300

    Ajuste da estrutura de pastas. Atualização da versão do JUnit.

---
+++ src/main/java/br/pucrs/alav/Recursion.java
@@ -0,0 +1,7 @@
+package br.pucrs.alav;
+
+public class Recursion {
+	public static void main(String[] args) {
+		System.out.println("Teste");
+	}
+}
---
+++ src/test/java/br/pucrs/alav/RecursionTests.java
@@ -0,0 +1,15 @@
+package br.pucrs.alav;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class RecursionTests {
+
+	@Test
+	public void test() { 
+		assertTrue(true, "Será?");
+	}
+
+}
